6165586d6f19473455404819792dca1086270e3b
Droi Developed <droi.developed@gmail.com>
2021-04-01 02:00:22 -0500

    Long polling add
    Se agrego una alternativa a websocket
    y se modifica el diseño del index

--- src/ui/js/index.js
+++ src/ui/js/index.js
@@ -1,10 +1,9 @@
 const ipcRenderer = require('electron').ipcRenderer;
 let data_user
 let Array_Pendings = [];
+let Array_LastConsult = [];
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  GetPendingSupports();
-  // localStorage.setItem('User_Code' , "Prueba") ;
   if ( localStorage.getItem('user_data')  == null || localStorage.getItem('user_data')  ==  "" ) {
     console.log('sin usuario');
     ipcRenderer.send('Main_Channel' , 'Login_window');
@@ -18,16 +17,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
       ipcRenderer.send('Main_Channel' , 'Minimize_Index');
     });
   
-    document.getElementById("max-btn").addEventListener("click", function (e) {
-      ipcRenderer.send('Main_Channel' , 'Maximize_Index');
-      if ( document.getElementsByClassName("header_title")[0].classList.contains('block_drag') ) {
-        document.getElementsByClassName("header_title")[0].classList.remove('block_drag');
-        document.getElementsByClassName('header_title')[0].setAttribute('draggable', true);
-      } else {
-        document.getElementsByClassName("header_title")[0].classList.add('block_drag');
-        document.getElementsByClassName('header_title')[0].setAttribute('draggable', false);
-      }
-    });
+    // document.getElementById("max-btn").addEventListener("click", function (e) {
+    //   ipcRenderer.send('Main_Channel' , 'Maximize_Index');
+    //   if ( document.getElementsByClassName("header_title")[0].classList.contains('block_drag') ) {
+    //     document.getElementsByClassName("header_title")[0].classList.remove('block_drag');
+    //     document.getElementsByClassName('header_title')[0].setAttribute('draggable', true);
+    //   } else {
+    //     document.getElementsByClassName("header_title")[0].classList.add('block_drag');
+    //     document.getElementsByClassName('header_title')[0].setAttribute('draggable', false);
+    //   }
+    // });
   
     document.getElementById("close-btn").addEventListener("click", function (e) {
       ipcRenderer.send('Main_Channel' , 'Close_Index');
@@ -39,19 +38,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
 ipcRenderer.on('Index_Channel' , (event , arg) => {
   if (arg == 'Login_Success') {
     load_user();
-    GetPendingSupports();
-    
   }
 })
 
 
 const GetPendingSupports = async (Id_Advisor) => {
-  console.log("Getting data pending...");
-  const request = await fetch("https://www.gesadmin.co/licenses/apidroi/public/apr/supports/pendings/"+Id_Advisor);
-  const data = await request.json();
-  Array_Pendings = data;
-  List_PendingSupports();
-  console.log(Array_Pendings);
+  console.log("Getting data pending...")
+
+  const request = await fetch("https://www.gesadmin.co/licenses/apidroi/public/apr/supports/pendings/"+Id_Advisor)
+  .catch(function(error) {
+    console.log('Hubo un problema con la petición Fetch:' + error.message);
+  });
+  
+  if (request.status == 502) {
+    GetPendingSupports(data_user.Code_Adviser);
+  }
+
+  const data = await request.json()
+  Array_Pendings = data
+
+  if (JSON.stringify(Array_LastConsult) !== JSON.stringify(Array_Pendings)) {
+    console.log('Nuevos datos');
+    Array_LastConsult = data
+    Array_Pendings = data
+
+    notifyMe("New_Support" , {Body: "..." , Tittle: "Nuevo soporte"})
+
+    if ( Array.isArray(Array_Pendings) ) {
+      List_PendingSupports();
+    }else{
+      document.getElementById('list_pendings').innerHTML = Array_Pendings.status.text
+    }
+
+  }else{
+    Array_LastConsult = data
+  }
+
+
+  
+
 };
 
 function closeSession (){
@@ -66,34 +91,57 @@ function load_user(){
   data_user = JSON.parse(localStorage.getItem('user_data'));
   document.getElementById('user_name').innerHTML = data_user.Treatment +" "+ data_user.First_Name +" "+ data_user.Surname;
   document.getElementById('user_rol').innerHTML = data_user.Name_Roll;
+
+  // Reload de soportes pendientes 
+  setInterval(() => {
+    GetPendingSupports(data_user.Code_Adviser);
+  }, 5000)
+  
 }
 
 function List_PendingSupports (){
+  let info_show = "";
   Array_Pendings.forEach(element => {
-    document.getElementById('list_pendings').innerHTML = `
-    <div class="card-body">
-      <span class="card-date"> ${moment(parseInt(element.Date)*1000).format('D/M/Y - H:mm')} </span>
-      <h4 class="card-title">Empresa </h4>
-  
-      <div class="form-group">
-        <label for="" class="control-label"> <strong>Asunto:</strong> Logo impresora </label>
-      </div>
-  
-      <div class="form-group">
-        <label for="" class="control-label "> <strong>Estado:</strong> </label>
-        <div class="" >
-          <select name="" id="" class="form-control">
-            <option value="Pending">Pendiente</option>
-            <option value="Pending">Finalizado</option>
-          </select>  
+     info_show = info_show+`
+
+    <div class="container-blocks pendings-block">
+        <div class="p-block-1">
+          <span class="" > <kbd>${element.Id}</kbd> </span>
         </div>
-        
-        <br>
-  
-        <button type="button"  id="new_window">Launch modal</button>
-      </div>   
+        <div class="p-block-2">
+          <p> ${element.Business_Name} </p>
+          <p> <b>  ${element.Description} </b> </p>
+        </div>
+        <div class="p-block-3">
+          <i class="fas fa-arrow-circle-right"></i>
+        </div>
+      </div>
   `;  
   });
+
+  document.getElementById('list_pendings').innerHTML = info_show;
   
+}
+
+function  notifyMe(Type_Alert , data_Alert)  {  
+  let Tittle = "";
+  let Body = "";
+  if (Type_Alert == 'New_Support') {
+    Body = data_Alert.Body;
+    Tittle = data_Alert.Tittle;
+  }
+  if  (!("Notification"  in  window))  {   
+      alert("Este navegador no soporta notificaciones de escritorio");  
+  }  
+  else  if  (Notification.permission  ===  "granted")  {
+      var  options  =   {
+          body:   Body,
+          icon:   (__dirname)+"/src/ui/img/ProfileDefault.png",
+          // dir :   "ltr"
+      };
+      var  notification  =  new  Notification(Tittle, options);
+  }
+    
+
 }
 
